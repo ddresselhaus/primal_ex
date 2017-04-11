4a6c3713f42b2364b994a8601fc733333b0d2872
Daniel Dresselhaus <ddresselhaus@gmail.com>
2017-04-11 16:52:43 -0400

    Initial commit - mix new

---
+++ lib/prime_ex.ex
@@ -0,0 +1,18 @@
+defmodule PrimeEx do
+  @moduledoc """
+  Documentation for PrimeEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PrimeEx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/prime_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule PrimeExTest do
+  use ExUnit.Case
+  doctest PrimeEx
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
