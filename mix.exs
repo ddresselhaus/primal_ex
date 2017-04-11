22376aafc078cb41f14cb484b377c4c6fc1e5f14
Daniel Dresselhaus <ddresselhaus@gmail.com>
2017-04-11 19:56:41 -0400

    Fix mixfile deps

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PrimalEx.Mixfile do
 
   def project do
     [app: :primal_ex,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -33,8 +33,8 @@ defmodule PrimalEx.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:benchfella, "~> 0.3.0"},
-      {:rustler, "~> 0.9.0", only: :dev},
+      {:benchfella, "~> 0.3.0", only: :dev},
+      {:rustler, "~> 0.9.0"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
